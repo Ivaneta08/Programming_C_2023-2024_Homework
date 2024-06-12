14f803a3a84500d9ebc572d745fa7fb48d9230a8
Ivaneta Ivanova <ivanetaivanova0908@gmail.com>
2024-06-12 11:53:24 +0300

    Homework1 done

--- Homework18_bin_files/work_with_citizen_bin_file.c
+++ Homework18_bin_files/work_with_citizen_bin_file.c
@@ -20,16 +20,43 @@ void menu()
 
 void printCitizens(DynamicArray * arr) {
     for (int i = 0; i < arr->size; i++) {
-        printf("%ld %s %d %d %d %d\n", 
-        arr->buffer[i].egn, arr->buffer[i].names, arr->buffer[i].age,
-        arr->buffer[i].education, arr->buffer[i].work_status, arr->buffer[i].marital_status);
+        printf("EGN: %ld\nNames: %s\nAge: %d\nEducation: ", 
+        arr->buffer[i].egn, arr->buffer[i].names, arr->buffer[i].age);
+
+        if(arr->buffer[i].education == 0)printf("None\n");
+        else if(arr->buffer[i].education == 1)printf("Primary\n");
+        else if(arr->buffer[i].education == 2)printf("Secondary\n");
+        else printf("Higher");
+        
+        printf("Work status: ");
+        if(arr->buffer[i].work_status == 0)printf("Unemployed\n");
+        else printf("Employed\n");
+
+        printf("Marital status: ");
+        if(arr->buffer[i].marital_status == 0)printf("Single\n");
+        else printf("Married\n");
+
+        printf("\n");
     }
-    printf("\n");
+}
+
+void saveCitizens(FILE *file, DynamicArray *arr) {
+    rewind(file);
+    for (int i = 0; i < arr->size; i++) {
+        fwrite(&arr->buffer[i].egn, sizeof(long), 1, file);
+        fwrite(&arr->buffer[i].namelen, sizeof(int), 1, file);
+        fwrite(&arr->buffer[i].names, sizeof(char), arr->buffer[i].namelen, file);
+        fwrite(&arr->buffer[i].age, sizeof(int), 1, file);
+        fwrite(&arr->buffer[i].education, sizeof(int), 1, file);
+        fwrite(&arr->buffer[i].work_status, sizeof(int), 1, file);
+        fwrite(&arr->buffer[i].marital_status, sizeof(int), 1, file);
+    }
+    fflush(file);
 }
 
 int main(void)
 {
-    FILE * file = fopen("citizens_of_Bulgaria.bin", "a+");
+    FILE * file = fopen("citizens_of_Bulgaria.bin", "ab+");
 
     if(file == NULL)
     {
@@ -40,18 +67,20 @@ int main(void)
     DynamicArray citizens = init(3);
 
     Citizen citizen;
-    while(fread(&citizen.egn, sizeof(long), 1, file) != EOF);
+    while(fread(&citizen.egn, sizeof(long), 1, file) == 1)
     {
         fread(&citizen.namelen, sizeof(citizen.namelen), 1, file);
-        fread(&citizen.names, sizeof(citizen.names), namelen, file);
+        
+        fread(&citizen.names, sizeof(char), citizen.namelen, file);
+        citizen.names[citizen.namelen] = '\0';
+        
         fread(&citizen.age, sizeof(citizen.age), 1, file);
         fread(&citizen.education, sizeof(citizen.education), 1, file);
         fread(&citizen.work_status, sizeof(citizen.work_status), 1, file);
         fread(&citizen.marital_status, sizeof(citizen.marital_status), 1, file);
+        
         pushBack(&citizens, citizen);
     }
-
-    fclose(file);
 
     int end = 1;
 
@@ -60,7 +89,8 @@ int main(void)
         menu();
         int choice;
         scanf("%d", &choice);
-        printf("\n")
+        //getchar();
+        printf("\n");
 
         switch (choice)
         {
@@ -73,16 +103,19 @@ int main(void)
             printf("Enter the EGN number of the citizen you wish to be deleted: ");
             long num;
             scanf("%ld", &num);
+            //getchar();
 
             int deleted = 0;
 
             for (int i = 0; i < citizens.size; i++) {
-                if(citizens.buffer[i].EGN == num)
+                if(citizens.buffer[i].egn == num)
                 {
-                    deleted = removeElement(&citizens, (uint)i);
+                    deleted = 1;
+                    removeElement(&citizens, (uint)i);
+                    break;
                 }
             }
-
+            
             if(deleted == 0)
             {
                 printf("Citizen not found.\n");
@@ -90,6 +123,7 @@ int main(void)
             else
             {
                 printf("Citizen removed.\n");
+                saveCitizens(file, &citizens);
             }
 
             printf("\n");
@@ -102,27 +136,43 @@ int main(void)
 
             printf("EGN: ");
             scanf("%ld", &new_citizen.egn);
+            getchar();
             
             printf("Names: ");
             fgets(new_citizen.names, 255, stdin);
 
+            int index = strcspn(new_citizen.names, "\n");
+            new_citizen.names[index] = '\0';
+
+            new_citizen.namelen = strlen(new_citizen.names);
+
             printf("Age: ");
             scanf("%d", &new_citizen.age);
+            getchar();
 
-            printf("Education (NONE = 0, PRIMARY = 1, SECONDARY = 2, HIGHER = 3): ");
-            scanf("%d", &new_citizen.education);
+            do{
+                printf("Education (NONE = 0, PRIMARY = 1, SECONDARY = 2, HIGHER = 3): ");
+                scanf("%d", &new_citizen.education);
+                getchar();
+            }while(new_citizen.education > 3 || new_citizen.education < 0);
 
-            printf("Work status: UNEMPLOYED = 0, EMPLOYED = 1");
-            scanf("%d", &new_citizen.work_status);
+            do{
+                printf("Work status: UNEMPLOYED = 0, EMPLOYED = 1): ");
+                scanf("%d", &new_citizen.work_status);
+                getchar();
+            }while(new_citizen.work_status > 1 || new_citizen.work_status < 0);
 
-            printf("Marital status: SINGLE = 0, MARRIED = 1");
-            scanf("%d", &new_citizen.marital_status);
-
-            new_citizen.namelen = strlen(new_citizen.names)
+            do{
+                printf("Marital status: SINGLE = 0, MARRIED = 1): ");
+                scanf("%d", &new_citizen.marital_status);
+                getchar();
+            }while(new_citizen.marital_status > 1 || new_citizen.marital_status < 0);
 
             printf("\n");
 
-            pushBack(&citizen, new_citizen);
+            pushBack(&citizens, new_citizen);
+
+            saveCitizens(file, &citizens);
             
             break;
 
@@ -136,6 +186,8 @@ int main(void)
             break;
         }
     }
+
+    fclose(file);
 
     return 0;
 }
